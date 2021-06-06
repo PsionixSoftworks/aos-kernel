e913b28d493f66a20d941f14e9beb321a2b749bd
gamemaker90 <gamemaker80@gmail.com>
2021-06-06 09:49:36 -0400

    Actually added 'stdio/getchar.c' this time...

---
+++ stdio/getchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+extern char key_buffer[256];
+
+/* Get the last char key press from the key buffer */
+int
+getchar(void)
+{
+	char buffer = 0;
+	if (key_buffer == '\0')
+		return 0;
+	buffer = key_buffer[0];
+	key_buffer[0] = '\0';
+	return buffer;
+}
